2c61fea5bdda798ed03da1db3c14afb066032533
zhoubin <zhoubin@hujiang.com>
2019-04-10 16:26:44 +0800

    update swagger info

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 // @APIVersion 1.0.0
-// @Title beego Test API
+// @Title bootgo Test API
 // @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
-// @TermsOfServiceUrl http://beego.me/
-// @License Apache 2.0
-// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+// @Contact zhoubin296@gmail.com
+// @TermsOfServiceUrl https://gitee.com/ipanocloud/bootgo
+// @License MIT
+// @LicenseUrl https://gitee.com/ipanocloud/bootgo/blob/master/LICENSE
 package routers
 
 import (
